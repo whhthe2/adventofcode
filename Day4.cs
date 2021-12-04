0162269c0486cc0b899ff3a09486cddd454a9add
wholland <wholland@wbgames.com>
2021-12-04 15:39:46 -0500

    run game loop, track marks, check for victory

--- Day4.cs
+++ Day4.cs
@@ -7,16 +7,99 @@ namespace adventofcode2021
 {
     internal class BingoBoard
     {
-        private const int size = 5;
-        private int[][] board;
-        public int[] this[int index]
+        // size of the board. the board is square.
+        public const int size = 5;
+
+        // the array of numbers on your board
+        private int[,] board;
+
+        // marks indicate which cells have a bingo chip on them
+        public bool[,] marks;
+
+        public bool HasFiveInARow
         {
-            get => board[index];
-            set => board[index] = value;
+            get 
+            {
+                bool inRow = true;
+                bool inCol = true;
+                for (int y=0; y<size; y++)
+                {
+                    var row = new bool[size];
+                    for (int x=0; x<size; x++)
+                    {
+                        row[y]=marks[x,y];
+                    }
+                    Console.WriteLine($"row {row}");
+                    
+                    foreach(var b in row)
+                    {
+                        if (b == false)
+                        {
+                            inRow = false;
+                            break;
+                        }
+                    }
+                    if (inRow)
+                    {
+                        return true; //winner
+                    }
+                }
+
+                for (int x=0; x< size; x++)
+                {
+                    var col = new bool[size];
+                    for (int y=0; y<size; y++)
+                    {
+                        col[x] = marks[x,y];
+                    }
+                    Console.WriteLine($"col {col}");
+                    
+                    foreach(var b in col)
+                    {
+                        if (b == false)
+                        {
+                            inCol = false;
+                            break;
+                        }
+                    }
+                    if (inCol)
+                    {
+                        return true; //winner
+                    }
+                }
+                return false;
+            }
+        }
+
+        public int score
+        {
+            get
+            {
+                int scoreTicker = 0;
+                for(int y=0;y<size;y++)
+                {
+                    for (int x=0;x<size;x++)
+                    {
+                        //only unmarked spaces on the board count
+                        if (!marks[x,y])
+                        {
+                            scoreTicker += board[x,y];
+                        }
+                    }
+                }
+                return scoreTicker;
+            }
+        }
+
+        public int this[int r, int c]
+        {
+            get => board[r, c];
+            set => board[r, c] = value;
         }
         public BingoBoard(string[] rows)
         {
-            board = new int[size][size];
+            board = new int[size,size];
+            marks = new bool[size,size];
             int rowId = 0;
             foreach (var r in rows)
             {
@@ -28,11 +111,10 @@ namespace adventofcode2021
                 int colId = 0;
                 foreach (var c in cols)
                 {
-                    board[rowId][colId] = int.Parse(c);
+                    board[rowId,colId] = int.Parse(c);
+                    //marks[rowId,colId] = false;
                     colId++;
                 }
-
-                board[rowId] = r;
                 rowId++;
             }
         }
@@ -40,21 +122,80 @@ namespace adventofcode2021
     internal static class Day4
     {
         private static Queue<int> valueSequence;
+        private static List<BingoBoard> boards;
         public static void Part1(string input)
         {
-            string[] gameBoards;
-            var rawValueSequence = ExtractValueSequence(input, out gameBoards );
+            //arrange the numbers to draw
+            string[] gameBoardsRaw;
+            var rawValueSequence = ExtractValueSequence(input, out gameBoardsRaw );
             var splitValueSequence = rawValueSequence.Split(",");
             valueSequence = new Queue<int>();
             foreach (var s in splitValueSequence)
             {
-                valueSequence.Append(int.Parse(s));
+                valueSequence.Enqueue(int.Parse(s));
             }
 
             //organize boards
-            foreach (var s in gameBoards)
+            int rowId = 0;
+            string[] currentBoard = null;
+            boards = new List<BingoBoard>();
+            foreach (var s in gameBoardsRaw)
             {
-                Console.WriteLine(s);
+                if (string.IsNullOrEmpty(s))
+                {
+                    if (currentBoard != null)
+                    {
+                        boards.Add(new BingoBoard(currentBoard));
+                    }
+                    currentBoard = new string[BingoBoard.size];
+                    rowId = 0;
+                    continue;
+                }
+                currentBoard[rowId] = s;
+                rowId++;
+            }
+            currentBoard = null; //ready for gc
+            
+            //start drawing values and marking boards;
+            while (valueSequence.Count > 0)
+            {
+                // get the next number
+                int drawnValue = valueSequence.Dequeue();
+                
+                // for tracking tiebreaker candidates;
+                List<BingoBoard> winningBoards = new List<BingoBoard>();
+
+                //look for boards with matches, mark them, and check for victory
+                foreach (var board in boards)
+                {
+                    for (int y=0; y<BingoBoard.size; y++)
+                    {
+                        for (int x=0; x<BingoBoard.size; x++)
+                        {
+                            if (drawnValue == board[x,y]) // match!
+                            {
+                                board.marks[x,y] = true;
+                                //check for victory
+                                if (board.HasFiveInARow)
+                                {
+                                    winningBoards.Add(board);
+                                }
+                            }
+                        }
+                    }
+                }
+
+                // finally, compare winning boards for the one with the best score
+                int bestScore = 0;
+                BingoBoard winningBoard = null;
+                foreach (var board in winningBoards)
+                {
+                    if (board.score > bestScore)
+                    {
+                        winningBoard = board;
+                    }
+                }
+                Console.WriteLine($"winning score: {winningBoard.score}");
             }
         }
         public static string ExtractValueSequence(string input, out string[] remainingValues)
