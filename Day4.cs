ef9f8e3fddb7d92eb13b748da415a1d888892e3c
Will Holland <whhthe2@gmail.com>
2021-12-04 11:52:52 -0500

    develop bingo board class, guard vs empty input

--- Day4.cs
+++ Day4.cs
@@ -5,16 +5,19 @@ using System.Linq;
 
 namespace adventofcode2021
 {
-    public class BingoBoard
+    internal class BingoBoard
     {
-        public BingoBoard()
+        private const int size = 5;
+        public BingoBoard(string[] rows)
         {
-            
+
         }
-        public string this[int index];
+
+        private int[][] board;
+        public int[] this[int index]
         {
-            get;
-            set;
+            get => board[index];
+            set => board[index] = value;
         }
     }
     internal static class Day4
@@ -29,21 +32,21 @@ namespace adventofcode2021
             foreach (var s in splitValueSequence)
             {
                 valueSequence.Append(int.Parse(s));
-                Console.WriteLine(s);  
             }
 
             //organize boards
             
             foreach (var s in gameBoards)
             {
-                
+                Console.WriteLine(s);
             }
-
-
         }
-
         public static string ExtractValueSequence(string input, out string[] remainingValues)
         {
+            if (String.IsNullOrEmpty(input))
+            {
+                throw new Exception ("cannot extract from empty input");
+            }
             var splitInput = input.Split("\n");
             remainingValues = splitInput[1..^1];
             return splitInput[0];
