76f0366b087b43d959f504a08d9d1fbacd524515
wholland <wholland@wbgames.com>
2021-12-10 13:59:15 -0500

    not gonna do 8... yet?

---
+++ Day8.cs
@@ -0,0 +1,24 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+/*
+using NUnit.Framework;
+from dotnet cli--
+dotnet add package Microsoft.NET.Test.Sdk
+dotnet add package Nunit3TestAdapter
+dotnet add package NUnit
+*/
+
+namespace adventofcode2021
+{   
+    public static class Day8
+    {
+        public static void Part1(string input)
+        {
+            Console.WriteLine(input);
+            // nope!
+        }
+    }
+}
