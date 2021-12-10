95d2b71fd1b320762c3ea4b73500e0f9b99d8e99
wholland <wholland@wbgames.com>
2021-12-10 11:48:54 -0500

    more sophisticated commandline features

--- Program.cs
+++ Program.cs
@@ -4,27 +4,52 @@ using System.CommandLine;
 using System.CommandLine.Invocation;
 using System.Net;
 using System.Text;
+using System.Reflection;
+
 namespace adventofcode2021
 {
     class Program
     {
+        #nullable enable
         static void Main(string[] args)
         {
-            /*var rootCmd = new RootCommand
+            var rootCmd = new RootCommand
             {
-                new Option<string>( "--input", "if provided, get input directly from the commandline" ),
-                new Option<int>( "--year", "the year. Must also provide -d."),
-                new Option<int>( "--day", "the day. Must also provide -y" )
-            };*/
-
-            var input = GetOrCacheAdventInput(2021, 7);
-            var testInput = "16,1,2,0,4,2,7,1,2,14";
-
-            Day7.Part1(input);
-
+                new Option<string?>( "--input", "if provided, get input directly from the commandline" ),
+                new Option<uint?>( "--year", "the year." ),
+                new Option<uint?>( "--day", "the day." )
+            };
+            rootCmd.Handler = CommandHandler.Create<string, uint?, uint?, IConsole>(Solve);
+            rootCmd.InvokeAsync(args);
         }
 
-        public static string GetOrCacheAdventInput(int year, int day, int part=1)
+        public static void Solve(string input, uint? year, uint? day, IConsole console)
+        {
+            uint selectedYear = year.HasValue ? year.Value : (uint)DateTime.Now.Year;
+            uint selectedDay = day.HasValue ? day.Value : (uint)DateTime.Now.Day;
+
+            string rawInput = string.IsNullOrEmpty(input) ? rawInput = GetOrCacheAdventInput(selectedYear, selectedDay) : input;
+            
+            string puzzleTypeName = $"adventofcode2021.Day{selectedDay}";
+            Type? puzzle = Type.GetType(puzzleTypeName);
+            if (puzzle == null)
+            {
+                throw new Exception($"cannot find type for {puzzleTypeName}.");
+            }
+
+            string solverMethodName = "Solve";
+            MethodInfo? solver = puzzle.GetMethod(solverMethodName, BindingFlags.Static | BindingFlags.Public);
+            if (solver == null)
+            {
+                throw new Exception($"cannot find public, static method named {solverMethodName}");
+            }
+
+            //Invoke the puzzle solver method
+            var solution = solver.Invoke(null, new object[] { rawInput });
+        }
+        #nullable disable
+        
+        public static string GetOrCacheAdventInput(uint year, uint day, int part=1)
         {
             var userDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "AdventOfCode");
             var cacheDirectory = Path.Combine(userDirectory, $"{year}", $"{day}");
